178b2ce312f9c4b37352d94293e5453fe1d28e57
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2019-12-10 10:35:54 +0100

    Split router into modules

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 let express = require('express');
-let apiRoutes = require("./api-routes");
+let sensorRoutes = require('./routers/sensorRoutes');
+let sensorDataRoutes = require('./routers/sensorDataRoutes');
+let sensorPackageRoutes = require('./routers/sensorPackageRoutes');
+let userRoutes = require('./routers/userRoutes');
+let messagingRoutes = require('./routers/messagingRoutes');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let mqttHandler = require("./messaging/mqttHandler.js");
 let config = require('../config.json');
 
 let app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use('/api', apiRoutes);
+
+app.use('/api/sensorPackages/', sensorPackageRoutes);
+app.use('/api/sensors/', sensorRoutes);
+app.use('/api/sensorData/', sensorDataRoutes);
+app.use('/api/users/', userRoutes);
+app.use('/api', messagingRoutes);
 
 const mqttHandle = new mqttHandler(config.flespiHost, config.flespiToken);
 mqttHandle.connect(config.sensorTopic);
 
-apiRoutes.setMqttClient(mqttHandle.mqttClient);
+messagingRoutes.setMqttClient(mqttHandle.mqttClient);
 
 mongoose.connect(config.url + config.db, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
